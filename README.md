7e3a1cc53ce87a38f9a1aa33018e67858159f342
Sam Tuson <sam.tuson@hotmail.co.uk>
2022-12-16 12:00:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Fifteensquared Spoiler
+
+Hides all answers and explanations for cryptic crosswords on [Fifteensquared](https://fifteensquared.net/). This script aims to let you find help with parsing an answer you found, or get a single answer while you're completely stumped without accidentally seeing other answers before you're ready.
+
+## Requirements
+
+-   A userscript manager such as [Tampermonkey](https://www.tampermonkey.net/)
+
+## Installation
+
+-   Open the [latest user script](src/fifteen-squared-spoiler.user.js?raw=1)
+-   Click install
+
+## Usage
+
+Click on a blocked-out answer to reveal it. Click again to block it out again. Answers and explanations can be revealed separately. Use the "reveal all" and "block all" buttons to control all items at once.
+
+Your browser will save which answers you have previously revealed if you close the page and come back later.
---
+++ src/fifteensquared-spoiler.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Fifteensquared Spoiler
+// @namespace    https://github.com/stuson/fifteensquared-spoiler
+// @homepageURL  https://github.com/stuson/fifteensquared-spoiler
+// @downloadURL  https://github.com/stuson/fifteensquared-spoiler/releases/latest/download/fifteensquared-spoiler.user.js
+// @updateURL    https://github.com/stuson/fifteensquared-spoiler/releases/latest/download/fifteensquared-spoiler.user.js
+// @version      0.1
+// @description  Hides all answers and explanations for cryptic crosswords on Fifteensquared behind toggleable spoilers.
+// @author       Sam Tuson
+// @match        https://www.fifteensquared.net/*/*/*/*/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @license      MIT
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    "use strict";
+
+    console.log("wew lad");
+})();
